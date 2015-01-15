4cdb0334fbe260b561fff1b1618b23cc827b0c51
Seppe Stas <seppe.stas@fifthplay.com>
2015-01-15 14:12:50 +0100

    Created some quick and dirty benchmarks

---
+++ dicom_test.go
@@ -0,0 +1,50 @@
+package dicom
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+var files [][]byte
+
+func init() {
+	f, err := filepath.Glob("examples/*.dcm")
+	if err != nil {
+		fmt.Println("failed to glob files")
+		panic(err)
+	}
+
+	for _, path := range f {
+		file, err := ioutil.ReadFile(path)
+		if err != nil {
+			fmt.Println("failed to read file")
+			panic(err)
+		}
+		files = append(files, file)
+
+	}
+}
+
+func BenchmarkParseSingle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Parse(files[0])
+		if err != nil {
+			fmt.Println("failed to parse dicom file")
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkParseMultiple(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, file := range files {
+			_, err := Parse(file)
+			if err != nil {
+				fmt.Println("failed to parse dicom file")
+				panic(err)
+			}
+		}
+	}
+}

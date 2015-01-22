040b843198f1d781660e246ff3b22c03f80326e5
Gilles De Mey <gilles.de.mey@gmail.com>
2015-01-22 02:15:20 +0100

    Added table formatter and silent option

--- bin/dicom.go
+++ bin/dicom.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"github.com/gillesdemey/go-dicom"
 	"io/ioutil"
+	"os"
+	tw "text/tabwriter"
 )
 
 var (
-	file = flag.String("file", "IM-0001-0001.dcm", "the DICOM file you want to parse")
+	file   = flag.String("file", "IM-0001-0001.dcm", "the DICOM file you want to parse")
+	silent = flag.Bool("silent", false, "wether or not to print all Data Elements")
 )
 
 func init() {
@@ -34,8 +37,17 @@ func main() {
 		fmt.Println(err)
 	}
 
-	for _, elem := range data.Elements {
-		fmt.Printf("%+v\n", elem)
+	if *silent == false {
+		// func NewWriter(output io.Writer, minwidth, tabwidth, padding int, padchar byte, flags uint) *Writer
+		table := tw.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
+
+		fmt.Fprintf(table, "Group\tElement\tName\tVR\tVL\tValue\n")
+
+		for _, elem := range data.Elements {
+			fmt.Fprintf(table, "%04X\t%04X\t%s\t%s\t%d\t%v\n", elem.Group, elem.Element, elem.Name, elem.Vr, elem.Vl, elem.Value)
+		}
+
+		table.Flush()
 	}
 
 }

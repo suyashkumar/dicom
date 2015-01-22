3f79b0c5b02fc3631aa7a496d65d6d6977eb439d
Gilles De Mey <gilles.de.mey@gmail.com>
2015-01-22 01:03:05 +0100

    Removed example and added CLI executable

---
+++ bin/dicom.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"../"
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	file = flag.String("file", "IM-0001-0001.dcm", "the DICOM file you want to parse")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+
+	parser, err := dicom.NewParser()
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadFile(*file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := parser.Parse(bytes)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	elem, _ := data.LookupElement("PatientName")
+	name := elem.Value
+	fmt.Printf("Patient name: %s\n", name)
+
+}

07439855bc1884a51c5ee8cb3e2f330e0d7620eb
Suyash Kumar <suyashkumar2003@gmail.com>
2018-07-06 17:50:02 -0400

    Update utility tool, cleanup

--- dicomutil/dicomutil.go
+++ dicomutil/dicomutil.go
@@ -33,17 +33,17 @@ func main() {
 		dicomlog.SetLevel(math.MaxInt32)
 	}
 	path := flag.Arg(0)
-	data, err := dicom.ReadDataSetFromFile(path, dicom.ReadOptions{DropPixelData: !*extractImages})
-	if data == nil {
+	parsedData, err := dicom.ReadDataSetFromFile(path, dicom.ReadOptions{DropPixelData: !*extractImages})
+	if parsedData == nil {
 		log.Panic("Error reading %s: %v", path, err)
 	}
 	if *printMetadata {
-		for _, elem := range data.Elements {
+		for _, elem := range parsedData.Elements {
 			fmt.Printf("%v\n", elem.String())
 		}
 	}
 	if *extractImages {
-		for _, elem := range data.Elements {
+		for _, elem := range parsedData.Elements {
 			if elem.Tag == dicomtag.PixelData {
 				data := elem.Value[0].(dicom.PixelDataInfo)
 				if data.Encapsulated {
@@ -54,10 +54,14 @@ func main() {
 					}
 					wg.Wait()
 				} else {
+					x, y, err := getDimensions(parsedData)
+					if err != nil {
+
+					}
 					var wg sync.WaitGroup
 					for frameIndex, frame := range data.NativeFrames {
 						wg.Add(1)
-						go generateNativeImage(frame, frameIndex, &wg) // parse image as gray scale
+						go generateNativeImage(frame, frameIndex, x, y, &wg) // parse image as gray scale
 					}
 					wg.Wait()
 				}
@@ -74,14 +78,31 @@ func generateEncapsulatedImage(frame []byte, frameIndex int, wg *sync.WaitGroup)
 	fmt.Printf("%s: %d bytes\n", path, len(frame))
 }
 
-func generateNativeImage(frame [][]int, frameIndex int, wg *sync.WaitGroup) {
+func generateNativeImage(frame [][]int, frameIndex, x, y int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	i := image.NewGray16(image.Rect(0, 0, 512, 512)) //TODO(suyash): dont assume 512 x 512
+	i := image.NewGray16(image.Rect(0, 0, x, y))
 	for j := 0; j < len(frame); j++ {
-		i.SetGray16(j%512, j/512, color.Gray16{Y: uint16(frame[j][0])}) // for now, assume we're not overflowing uint16, assume gray image
+		i.SetGray16(j%x, j/y, color.Gray16{Y: uint16(frame[j][0])}) // for now, assume we're not overflowing uint16, assume gray image
 	}
 	name := fmt.Sprintf("image_%d.jpg", frameIndex)
-	f, _ := os.Create(name)
+	f, err := os.Create(name)
+	if err != nil {
+		fmt.Printf("Error while creating file: %s", err.Error())
+	}
 	jpeg.Encode(f, i, &jpeg.Options{Quality: 100})
 	fmt.Printf("%s written \n", name)
+}
+
+func getDimensions(d *dicom.DataSet) (x, y int, err error) {
+	rows, err := d.FindElementByTag(dicomtag.Rows)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	cols, err := d.FindElementByTag(dicomtag.Columns)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return int(cols.MustGetUInt16()), int(rows.MustGetUInt16()), nil
 }

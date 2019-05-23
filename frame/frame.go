96069df4e5df17f5178db0c0256e039665fb3faf
Suyash Kumar <suyashkumar2003@gmail.com>
2019-05-23 01:53:52 -0700

    update frame ordering

--- frame/frame.go
+++ frame/frame.go
@@ -1,5 +1,12 @@
 package frame
 
+// Frame wraps a single encapsulated or native image frame
+type Frame struct {
+	IsEncapsulated   bool
+	EncapsulatedData EncapsulatedFrame
+	NativeData       NativeFrame
+}
+
 // NativeFrame represents a native image frame
 type NativeFrame struct {
 	// Data is a slice of pixels, where each pixel can have multiple values
@@ -13,11 +20,4 @@ type NativeFrame struct {
 type EncapsulatedFrame struct {
 	// Data is a collection of bytes representing a JPEG encoded image frame
 	Data []byte
-}
-
-// Frame wraps a single encapsulated or native image frame
-type Frame struct {
-	IsEncapsulated   bool
-	EncapsulatedData EncapsulatedFrame
-	NativeData       NativeFrame
 }

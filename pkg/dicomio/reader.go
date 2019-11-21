b04df365c7b210717c19286ec8d860321c48801c
Suyash Kumar <suyashkumar2003@gmail.com>
2019-11-20 22:17:10 -0800

    Add readInt and readDate

--- pkg/dicomio/reader.go
+++ pkg/dicomio/reader.go
@@ -10,6 +10,8 @@ type Reader interface {
 	ReadN(n uint32) ([]byte, error)
 	ReadUInt16() (uint16, error)
 	ReadUInt32() (uint32, error)
+	ReadInt16() (int16, error)
+	ReadInt32() (int32, error)
 	ReadString(n uint32) (string, error)
 	Skip(n uint) error
 }

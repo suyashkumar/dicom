3e16541b4f097b99172d21b38729330a9f83c2ac
m75n <m75n@qq.com>
2020-01-15 15:08:04 +0800

    Update character encoding map (#54)

--- dicomio/charset.go
+++ dicomio/charset.go
@@ -67,6 +67,10 @@ var htmlEncodingNames = map[string]string{
 	"ISO_IR 166":      "iso-ir-166",
 	"ISO 2022 IR 166": "iso-ir-166",
 	"ISO 2022 IR 87":  "iso-2022-jp",
+	"ISO 2022 IR 58":  "iso-ir-58",
+	"ISO_IR 192":      "utf8",
+	"GB18030":         "gb18030",
+	"GBK":             "gbk",
 }
 
 // ParseSpecificCharacterSet converts DICOM character encoding names, such as

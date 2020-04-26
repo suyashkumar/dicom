cda37247242aaf685e7202c9d25415dd20170775
Suyash Kumar <suyashkumar2003@gmail.com>
2020-04-25 21:16:22 -0700

    Update dicomio.Reader interface, add in progress implementation (#68)

--- pkg/dicomio/reader.go
+++ pkg/dicomio/reader.go
@@ -14,13 +14,45 @@ type Reader interface {
 	ReadInt32() (int32, error)
 	ReadString(n uint32) (string, error)
 	Skip(n uint) error
+	PushLimit(n int)
+	PopLimit()
+	IsLimitExhausted() bool
 }
 
 type reader struct {
-	in        io.Reader
-	bo        binary.ByteOrder
-	limit     int64
-	bytesRead int64
+	in         io.Reader
+	bo         binary.ByteOrder
+	limit      int64
+	bytesRead  int64
+	limitStack []int64
 }
 
-// TODO: implement reader
+func NewReader(in io.Reader, bo binary.ByteOrder, limit int64) (Reader, error) {
+	return &reader{
+		in:        in,
+		bo:        bo,
+		limit:     limit,
+		bytesRead: 0,
+	}, nil
+}
+
+// TODO: Implement the rest of these interface functions
+func (r *reader) ReadN(n uint32) ([]byte, error) {
+	return nil, nil
+}
+
+func (r *reader) ReadUInt16() (uint16, error) {
+	var out uint16
+	err := binary.Read(r, r.bo, &out)
+	return out, err
+}
+
+func (r *reader) ReadUInt32() (uint32, error)         { return 0, nil }
+func (r *reader) ReadInt16() (int16, error)           { return 0, nil }
+func (r *reader) ReadInt32() (int32, error)           { return 0, nil }
+func (r *reader) ReadString(n uint32) (string, error) { return "", nil }
+func (r *reader) Skip(n uint) error                   { return nil }
+func (r *reader) Read(p []byte) (n int, err error)    { return r.in.Read(p) }
+func (r *reader) PushLimit(n int)                     {}
+func (r *reader) PopLimit()                           {}
+func (r *reader) IsLimitExhausted() bool              { return false }

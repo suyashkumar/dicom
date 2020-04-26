1b82e922680842177bd624aca7a94485509cecaa
Suyash Kumar <suyashkumar2003@gmail.com>
2020-04-25 22:18:28 -0700

    Implement initial Parser, explcit length SQ reader (#69)
    This change implements the initial, simplified Parser interface in parse.go, and also completes an implementation of parsing explicit length sequences.
    
    There are many TODOs that were inserted, but submitting this to continue rapidly towards an initial rewritten prototype that can parse simple dicoms successfully.

---
+++ parse.go
@@ -0,0 +1,117 @@
+package dicom
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+
+	"github.com/suyashkumar/dicom/pkg/dicomio"
+	"github.com/suyashkumar/dicom/pkg/tag"
+)
+
+const (
+	MagicWord = "DICM"
+)
+
+var (
+	ErrorMagicWord              = errors.New("error, DICM magic word not found in correct location")
+	ErrorMetaElementGroupLength = errors.New("MetaElementGroupLength tag not found where expected")
+)
+
+type Parser interface {
+	// Parse DICOM data
+	Parse() (Dataset, error)
+}
+
+type parser struct {
+	reader  dicomio.Reader
+	dataset Dataset
+	// file is optional, might be populated if reading from an underlying file
+	file *os.File
+}
+
+func NewParser(in io.Reader, bytesToRead int64) (Parser, error) {
+	reader, err := dicomio.NewReader(bufio.NewReader(in), binary.LittleEndian, bytesToRead)
+	if err != nil {
+		return nil, err
+	}
+
+	p := parser{
+		reader: reader,
+	}
+
+	elems, err := p.readHeader()
+	if err != nil {
+		return nil, err
+	}
+
+	p.dataset = Dataset{Elements: elems}
+
+	return nil, nil
+}
+
+func NewParserFromBytes(data []byte) (Parser, error) {
+	return nil, nil
+}
+
+// readHeader reads the DICOM magic header and group two metadata elements.
+func (p *parser) readHeader() ([]*Element, error) {
+	// Must read as LittleEndian explicit VR
+	err := p.reader.Skip(128) // skip preamble
+	if err != nil {
+		return nil, err
+	}
+
+	// Check DICOM magic word
+	if s, err := p.reader.ReadString(4); err != nil || s != MagicWord {
+		return nil, ErrorMagicWord
+	}
+
+	// Read the length of the metadata elements: (0002,0000) MetaElementGroupLength
+	maybeMetaLen, err := readElement(p.reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if maybeMetaLen.Tag != tag.FileMetaInformationGroupLength || maybeMetaLen.Value.ValueType() != Ints {
+		return nil, ErrorMetaElementGroupLength
+	}
+
+	metaLen := maybeMetaLen.Value.GetValue().(IntsValue).value[0]
+
+	metaElems := []*Element{maybeMetaLen} // TODO: maybe set capacity to a reasonable initial size
+
+	// Read the metadata elements
+	p.reader.PushLimit(metaLen)
+	defer p.reader.PopLimit()
+	for !p.reader.IsLimitExhausted() {
+		elem, err := readElement(p.reader)
+		if err != nil {
+			// TODO: see if we can skip over malformed elements somehow
+			return nil, err
+		}
+		metaElems = append(metaElems, elem)
+	}
+
+	return metaElems, nil
+}
+
+func (p *parser) Parse() (Dataset, error) {
+	for p.reader.IsLimitExhausted() {
+		// TODO: avoid silent looping
+		elem, err := readElement(p.reader)
+		if err != nil {
+			// TODO: tolerate some kinds of errors and continue parsing
+			return Dataset{}, err
+		}
+
+		// TODO: if we encounter a dicomtag.SpecificCharacterSet, update the reader to accommodate
+		// TODO: add dicom options to only keep track of certain tags
+
+		p.dataset.Elements = append(p.dataset.Elements, elem)
+	}
+
+	return Dataset{}, nil
+}

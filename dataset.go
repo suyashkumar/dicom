c57141716b46181262aad87c3800ae870f254dbc
Suyash Kumar <suyashkumar2003@gmail.com>
2020-07-18 19:15:41 -0700

    Implement initial FlatIterator for a Dataset (#97)
    This change implements a simple FlatIterator for a dataset which sends all elements in a Dataset to a channel, including elements nested inside Sequences. This also implements initial sanity tests and testable examples, along with a helper FindElementByTagNested function.

--- dataset.go
+++ dataset.go
@@ -22,3 +22,38 @@ func (d *Dataset) FindElementByTag(tag tag.Tag) (*Element, error) {
 	}
 	return nil, ErrorElementNotFound
 }
+
+// FindElementByTagNested searches through the dataset and returns a pointer to the matching element.
+// This call searches through a flat representation of the dataset, including within sequences.
+func (d *Dataset) FindElementByTagNested(tag tag.Tag) (*Element, error) {
+	for e := range d.FlatIterator() {
+		if e.Tag == tag {
+			return e, nil
+		}
+	}
+	return nil, ErrorElementNotFound
+}
+
+// FlatIterator returns a channel upon which every element in this Dataset will be sent,
+// including elements nested inside sequences.
+// Note that the sequence element itself is sent on the channel in addition to the child elements in the sequence.
+// TODO(suyashkumar): decide if the sequence element itself should be sent or not
+func (d *Dataset) FlatIterator() <-chan *Element {
+	elemChan := make(chan *Element)
+	go func() {
+		flatElementsIterator(d.Elements, elemChan)
+		close(elemChan)
+	}()
+	return elemChan
+}
+
+func flatElementsIterator(elems []*Element, elemChan chan<- *Element) {
+	for _, elem := range elems {
+		if elem.Value.ValueType() == Sequences {
+			for _, seqItem := range elem.Value.GetValue().([]*SequenceItemValue) {
+				flatElementsIterator(seqItem.elements, elemChan)
+			}
+		}
+		elemChan <- elem
+	}
+}

7a3c7368398ddb059b067acfd49f580fcf325b65
Suyash Kumar <suyashkumar2003@gmail.com>
2020-07-19 12:35:16 -0700

    Remove legacy directory of v0.* code. If needed, callers should use tagged versions to access the old version of the dicom library before the rewrite.

--- legacy/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM golang:1.13.4-alpine3.10
-WORKDIR /go/src/github.com/suyashkumar/dicom
-COPY . .
-RUN apk add --no-cache make git tar zip build-base
-RUN make
--- legacy/constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-// GoDICOMImplementationClassUIDPrefix defines the UID prefix for
-// dicom. Provided by https://www.medicalconnections.co.uk/Free_UID
-const GoDICOMImplementationClassUIDPrefix = "1.2.826.0.1.3680043.9.7133"
-
-var GoDICOMImplementationClassUID = GoDICOMImplementationClassUIDPrefix + ".1.1"
-
-const GoDICOMImplementationVersionName = "GODICOM_1_1"

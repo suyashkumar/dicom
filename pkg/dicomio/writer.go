fd193553493812481fe397f2f7e1d05ed65cd73d
Brian Hardell <brian@segmed.ai>
2020-08-26 16:18:19 -0700

    Finish outlining general structure of writeFileHeader() function and delegate functions. Haven't decided on writeMetaElem as a anonymous function vs. actual funcion yet
    Sponsored by Segmed Inc segmed.ai

--- pkg/dicomio/writer.go
+++ pkg/dicomio/writer.go
@@ -7,17 +7,33 @@ import (
 
 // Writer is  what Encoder object used to be
 type Writer interface {
+  SetTransferSynax(bo binary.ByteOrder)
 }
 
 type writer struct {
   out io.Writer
   bo binary.ByteOrder
+  implicit bool
 }
 
 // See https://github.com/suyashkumar/dicom/blob/6ffe547e2a080b3dcc1ce01946a7c7350d531bdc/dicomio/buffer.go#L80
-func NewWriter(out io.Writer, bo binary.ByteOrder, ...) *Writer {
+func NewWriter(out io.Writer, bo binary.ByteOrder, implicit bool) *Writer {
   return &Writer{
     out: out,
     bo: bo,
+    implicit: implicit,
   }
 }
+
+func (w *Writer) SetTransferSynax(bo binary.ByteOrder, implicit bool) {
+  w.bo = bo
+  w.implicit = implicit
+}
+
+// Retrieve the a []byte representation of what's contained in writer.out
+func (w *Writer) Bytes() []byte {return ni}
+
+
+// Low-level functions to write to writer.out
+func (w *Writer) WriteZeros() {}
+func (w *Writer) WriteZeros() {}
--- write.go
+++ write.go
@@ -29,7 +29,7 @@ func Write(out io.Writer, ds *Dataset, opts ...WriteOption) error {
 	}
 
 	// Write the file header with meta elements
-	err := writeFileHeader(&w, metaElems, opts...)
+	err := writeFileHeader(w, ds, metaElems, opts...)
 	if err != nil {
 		return err
 	}
@@ -44,7 +44,7 @@ func Write(out io.Writer, ds *Dataset, opts ...WriteOption) error {
 	// Write the rest of the elements with writeElement
 	for _, elem := range ds.Elements {
 		if elem.Tag != tag.MetadataGroup {
-			err = writeElement(&w, elem, opts...)
+			err = writeElement(w, elem, opts...)
 			if err != nil {
 				return err
 			}
@@ -74,12 +74,49 @@ func toOptSet(opts ...WriteOption) *writeOptSet {
 	return optSet
 }
 
-func writeFileHeader(w *dicomio.Writer, metaElems []*Element, opts ...WriteOption) error {
+func writeFileHeader(w *dicomio.Writer, ds *Dataset, metaElems []*Element, opts ...WriteOption) error {
 	w.SetTransferSynax(binary.LittleEndian, false) // TODO: either expand this or make this function
 
 	subWriter := dicomio.NewWriter(&bytes.Buffer{}, binary.LittleEndian, false)
 	tagsUsed := make(map[tag.Tag]bool)
 	tagsUsed[tag.FileMetaInformationGroupLength] = true
+
+	writeMetaElem := func(tag tag.Tag) error {
+		elem, err := ds.FindElementByTag(tag)
+		if err != nil {
+			return err
+		}
+		tagsUsed[tag] = true // TODO this hsouldn't actually be changed until writeElement returns nil for error
+		return writeElement(subWriter, elem, opts...)
+	}
+
+	writeMetaElem(dicomtag.FileMetaInformationVersion)
+	writeMetaElem(dicomtag.MediaStorageSOPClassUID)
+	writeMetaElem(dicomtag.MediaStorageSOPInstanceUID)
+	writeMetaElem(dicomtag.TransferSyntaxUID)
+	writeMetaElem(dicomtag.ImplementationClassUID)
+	writeMetaElem(dicomtag.ImplementationVersionName)
+
+	for _, elem := range metaElems {
+		if elem.Tag.Group == dicomtag.MetadataGroup {
+			if _, ok := tagsUsed[elem.Tag]; !ok {
+				err = writeElement(subEncoder, elem, opts...)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	metaBytes := subWriter.Bytes()
+	w.WriteZeros(128)
+	w.WriteString("DICM")
+	lengthElem  // TODO make lengthElem
+	err = writeElement(w, lengthElem, opts...) // TODO write metaelementgrouplength tag
+	if err != nil {
+		return err
+	}
+	w.WriteBytes(metaBytes)
 
 	return nil
 }
@@ -116,9 +153,23 @@ func writeElement(w *dicomio.Writer, elem *Elemet, opts ...WriteOption) error {
 	return nil
 }
 
-func writeMetaElem(w *Writer, tag tag.Tag, tagsUsed *map[tag.Tag]bool) error {
-
-}
+// func writeMetaElem(w *Writer, tag tag.Tag, tagsUsed *map[tag.Tag]bool) error {
+// 	tagInfo := tag.Find(tag)
+// 	elem := Element{
+// 		Tag: tag,
+// 		ValueRepresentation: tag.GetVRKind(tagInfo.VR),
+// 		RawValueRepresentation: tagInfo.VR,
+//
+//
+// 	}
+//
+// 	if elem, err := element.FindByTag(metaElems, tag); err == nil {
+// 			Element(subEncoder, elem, opts...)
+// 		} else {
+// 			subEncoder.SetErrorf("%v not found in metaelems: %v", dicomtag.DebugString(tag), err)
+// 		}
+// 		tagsUsed[tag] = true
+// }
 
 func verifyVR(elem *Element) error {
 	return nil

0e698053bf24cd3dea949d80d73d4e760bc90fa7
Billy Peake <peake100@gmail.com>
2020-12-23 16:54:06 -0800

    updated doc strings

--- pkg/pn/personName.go
+++ pkg/pn/personName.go
@@ -1,3 +1,7 @@
+/*
+The pn package provides methods and data types for inspecting Person Name (PN)
+Value Representations.
+ */
 package pn
 
 import (
@@ -5,8 +9,8 @@ import (
 	"regexp"
 )
 
-// Parses PersonName value from an element with a "PN" VR. See the "PN" entry at:
-// http://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html, and the
+// Parsed PersonName value information from an element with a "PN" VR. See the "PN"
+// entry at: http://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
 type PersonName struct {
 	// The person's family or last name.
 	FamilyName string
@@ -29,14 +33,17 @@ func (pn PersonName) String() string {
 		"^" + pn.NameSuffix
 }
 
+// Regex for parsing PN values.
 var pnRegex = regexp.MustCompile(
 	`(?P<Fist>.*)\^(?P<Last>.*)\^(?P<Middle>.*)\^(?P<Prefix>.*)\^(?P<Suffix>.*)`,
 )
 
+// ErrParsePersonName is returned when attempting to parse PersonName from a string.
 var ErrParsePersonName = errors.New("person name value does not match Dicom Spec" +
 	" '[Last]^[First]^[Middle]^[Prefix]^[Suffix]'")
 
-// Converts a Person Name string to parsed PersonName struct.
+// Converts a string from a dicom element with a Value Representation of PN to a
+// parsed PersonName struct.
 func NewPersonNameFromDicom(pnString string) (name PersonName, err error) {
 	// Run the regex against the name
 	matches := pnRegex.FindStringSubmatch(pnString)

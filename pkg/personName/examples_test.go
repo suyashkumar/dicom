e73e0789a2bd42045b69121786f6ff65935d5a00
Billy Peake <peake100@gmail.com>
2020-12-28 00:18:22 -0800

    example revisions

--- pkg/personName/examples_test.go
+++ pkg/personName/examples_test.go
@@ -8,35 +8,35 @@ import (
 // How to parse an existing PN value.
 func ExampleParse() {
 	// A raw PN string value with alphabetic, ideographic, and phonetic information.
-	pnRaw := "Potter^Harry^James^^=哈利^波特^詹姆^^=hɛər.i^pɒ.tər^dʒeɪmz^^"
+	rawPN := "Potter^Harry^James^^=哈利^波特^詹姆^^=hɛər.i^pɒ.tər^dʒeɪmz^^"
 
 	// To parse a PN value, use personName.Parse
-	pn, err := personName.Parse(pnRaw)
+	parsedPN, err := personName.Parse(rawPN)
 	if err != nil {
 		panic(err)
 	}
 
 	// PN values are broken into three groups: Alphabetic, Phonetic, and Ideographic.
 	// which can be accessed through getter methods.
-	fmt.Println("ALPHABETIC", pn.Alphabetic().String())
-	fmt.Println("IDEOGRAPHIC", pn.Ideographic().String())
-	fmt.Println("PHONETIC", pn.Phonetic().String())
+	fmt.Println("ALPHABETIC:", parsedPN.Alphabetic().String())
+	fmt.Println("IDEOGRAPHIC:", parsedPN.Ideographic().String())
+	fmt.Println("PHONETIC:", parsedPN.Phonetic().String())
 
 	// Each Group info in turn has Getter methods for retrieving individual data.
-	fmt.Println("FAMILY NAME:", pn.Alphabetic().FamilyName())
-	fmt.Println("GIVEN NAME: ", pn.Alphabetic().GivenName())
-	fmt.Println("MIDDLE NAME:", pn.Alphabetic().MiddleName())
-	fmt.Println("NAME PREFIX:", pn.Alphabetic().NamePrefix())
-	fmt.Println("NAME SUFFIX:", pn.Alphabetic().NameSuffix())
+	fmt.Println("FAMILY NAME:", parsedPN.Alphabetic().FamilyName())
+	fmt.Println("GIVEN NAME: ", parsedPN.Alphabetic().GivenName())
+	fmt.Println("MIDDLE NAME:", parsedPN.Alphabetic().MiddleName())
+	fmt.Println("NAME PREFIX:", parsedPN.Alphabetic().NamePrefix())
+	fmt.Println("NAME SUFFIX:", parsedPN.Alphabetic().NameSuffix())
 
 	// To print the original raw value, simply use the string method.
-	fmt.Println("ORIGINAL RAW:", pn.String())
+	fmt.Println("ORIGINAL RAW:", parsedPN.String())
 }
 
 // How to create new PN value.
 func ExampleNew() {
 	// Create a new PN like so
-	pn := personName.New(
+	pnVal := personName.New(
 		personName.NewGroupInfo(
 			"Potter",
 			"Harry",
@@ -46,9 +46,9 @@ func ExampleNew() {
 			// This PN group will render trailing separators.
 			false,
 		),
-		// Add empty group that will render it's separators.
+		// Add empty group that will render its separators.
 		personName.NewGroupEmpty(false),
-		// Add empty group that will render it's separators.
+		// Add empty group that will render its separators.
 		personName.NewGroupEmpty(false),
 		// Remove groups from string render that do not add information.
 		true,
@@ -57,10 +57,10 @@ func ExampleNew() {
 	// Print the string, should render as 'Potter^Harry^James^^'. Even though those
 	// groups were set to render trailing separators, because they are empty, they are
 	// suppressed.
-	fmt.Println("PN:", pn.String())
+	fmt.Println("PN 1:", pnVal.String())
 
-	// Now lets make one that still renders empty groups with String().
-	pn = personName.New(
+	// Now let's make one that still renders empty groups with String().
+	pnVal = personName.New(
 		personName.NewGroupInfo(
 			"Potter",
 			"Harry",
@@ -79,5 +79,5 @@ func ExampleNew() {
 	)
 
 	// This will render as 'Potter^Harry^James^^=^^^^=^^^^'
-	fmt.Println("PN:", pn.String())
+	fmt.Println("PN 2:", pnVal.String())
 }

b7c176581cf9ad9e97abf27f068845da84309bc4
Billy Peake <peake100@gmail.com>
2021-04-05 14:37:02 -0700

    added example

---
+++ pkg/dcmtest/examples_test.go
@@ -0,0 +1,33 @@
+package dcmtest_test
+
+import (
+	"github.com/suyashkumar/dicom"
+	"github.com/suyashkumar/dicom/pkg/dcmtest"
+	"testing"
+)
+
+func MyDatasetFunc(dataset dicom.Dataset) error {
+	return nil
+}
+
+func ExampleWalkIncludedFS() {
+	// For this example we will make a mock *testing.T value. Normally this would be
+	// passed into your testing function automatically by `go test`.
+	t := new(testing.T)
+
+	// Each file in dcmtest.IncludedFS will be run in it's own subtest automatically.
+	dcmtest.WalkIncludedFS(t, func(t *testing.T, tc dcmtest.FSTestCase, setupErr error) {
+		// On a setup error, we want to report it.
+		if setupErr != nil {
+			t.Fatal("setup error:", setupErr)
+		}
+
+		t.Log("TESTING:", tc.DCMPath)
+
+		// Pass the pared dataset to the function we actually want to test
+		err := MyDatasetFunc(tc.Dataset)
+		if err != nil {
+			t.Error("MyDatasetFunc error:", err)
+		}
+	})
+}

afd9fe185b246550a9bd7a6e4bb35c69b229e07a
Billy Peake <peake100@gmail.com>
2021-04-05 15:35:27 -0700

    moved parse examples back to original place

--- parse_test.go
+++ parse_test.go
@@ -13,6 +13,35 @@ import (
 	"github.com/suyashkumar/dicom/pkg/dcmtest"
 )
 
+// TestParse is an end-to-end sanity check over DICOMs in testdata/. Currently it only checks that no error is returned
+// when parsing the files.
+func TestParse(t *testing.T) {
+	// This will walk all of our test data and try parsing the Dataset, so we can simply
+	// report if we get passed an error.
+	dcmtest.WalkIncludedFS(t, func(t *testing.T, tc dcmtest.FSTestCase, setupErr error) {
+		if setupErr != nil {
+			t.Fatalf("setup error: %v", setupErr)
+		}
+	})
+}
+
+// BenchmarkParse runs sanity benchmarks over the sample files in testdata.
+func BenchmarkParse(b *testing.B) {
+	// This will walk all of our test data and run a sub-test for each one.
+	dcmtest.BenchIncludedFS(b, func(b *testing.B, tc dcmtest.FSTestCase, setupErr error) {
+		// The test helper will have already parsed the file once, so we will use that
+		// as validation that the dataset is good.
+		if setupErr != nil {
+			b.Fatalf("setup error: %v", setupErr)
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_, _ = dicom.Parse(tc.DCMFile, tc.DCMStat.Size(), nil)
+		}
+	})
+}
+
 func Example_readFile() {
 	// See also: dicom.Parse, which uses a more generic io.Reader API.
 	dataset, _ := dicom.ParseFile("./pkg/dcmtest/data/1.dcm", nil)
@@ -52,33 +81,4 @@ func Example_getImageFrames() {
 		_ = jpeg.Encode(f, img, &jpeg.Options{Quality: 100})
 		_ = f.Close()
 	}
-}
-
-// TestParse is an end-to-end sanity check over DICOMs in testdata/. Currently it only checks that no error is returned
-// when parsing the files.
-func TestParse(t *testing.T) {
-	// This will walk all of our test data and try parsing the Dataset, so we can simply
-	// report if we get passed an error.
-	dcmtest.WalkIncludedFS(t, func(t *testing.T, tc dcmtest.FSTestCase, setupErr error) {
-		if setupErr != nil {
-			t.Fatalf("setup error: %v", setupErr)
-		}
-	})
-}
-
-// BenchmarkParse runs sanity benchmarks over the sample files in testdata.
-func BenchmarkParse(b *testing.B) {
-	// This will walk all of our test data and run a sub-test for each one.
-	dcmtest.BenchIncludedFS(b, func(b *testing.B, tc dcmtest.FSTestCase, setupErr error) {
-		// The test helper will have already parsed the file once, so we will use that
-		// as validation that the dataset is good.
-		if setupErr != nil {
-			b.Fatalf("setup error: %v", setupErr)
-		}
-
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			_, _ = dicom.Parse(tc.DCMFile, tc.DCMStat.Size(), nil)
-		}
-	})
 }

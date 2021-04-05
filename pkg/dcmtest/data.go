46e40ff0a19f4439f2b8b60e6e2561832bcc7c1c
Billy Peake <peake100@gmail.com>
2021-04-05 15:11:57 -0700

    madde warnings their own hheadings

--- pkg/dcmtest/data.go
+++ pkg/dcmtest/data.go
@@ -4,13 +4,15 @@ import (
 	"embed"
 )
 
-// IncludedFS uses the embed package to read all of the test files in /pkg/dcmtest/data into
-// an embed.FS for testing. This allows us to keep the files in memory without reading
-// them from disk each time they are needed.
+// IncludedFS uses the embed package to read all of the test files in /pkg/dcmtest/data
+// into an embed.FS for testing. This allows us to keep the files in memory without
+// reading them from disk each time they are needed, or worrying about relative import
+// paths.
 //
-// WARNING: this var should only be used for testing purposes. It contains a number of
-// full DICOM files that could significantly bloat a binary if included in production
-// code.
+// WARNING
+//
+// This var should only be used for testing purposes. It contains a number of full DICOM
+// files that could significantly bloat a binary's size if included in production code.
 //
 //go:embed **/*.dcm
 var IncludedFS embed.FS
--- pkg/dcmtest/doc.go
+++ pkg/dcmtest/doc.go
@@ -1,3 +1,9 @@
 // Package dcmtest contains helper methods and functions for testing against dicom
 // files.
+//
+// WARNING
+//
+// This module should only be used by test files or test helper packages to be run by
+// `gotest`, as it included an embedded dataset which will lead to significant binary
+// size bloat when imported into production code.
 package dcmtest

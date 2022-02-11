bb50ebe640723d3c6d12c2a2ac6557fcddb382ab
jabillings <jabillings@users.noreply.github.com>
2022-02-11 13:18:27 -0800

    improve description
    Co-authored-by: Suyash Kumar <suyashkumar2003@gmail.com>

--- read_test.go
+++ read_test.go
@@ -367,7 +367,7 @@ func TestReadNativeFrames(t *testing.T) {
 			expectedError:     ErrorUnsupportedBitsAllocated,
 		},
 		{
-			Name: "3x3, 3 frames, 1 samples/pixel",
+			Name: "3x3, 3 frames, 1 samples/pixel, data bytes with padded 0",
 			existingData: Dataset{Elements: []*Element{
 				mustNewElement(tag.Rows, []int{3}),
 				mustNewElement(tag.Columns, []int{3}),

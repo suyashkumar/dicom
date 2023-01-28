d7360992a8392e88706fcd4efeac2a2b72a4ffa1
jesslatimer <97333535+jesslatimer@users.noreply.github.com>
2023-01-27 20:41:02 -0800

    Add Fuzz test (#252)

---
+++ fuzz/dicom_fuzz_test.go
@@ -0,0 +1,15 @@
+package fuzz
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/suyashkumar/dicom"
+)
+
+func FuzzDICOMParse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, test_file []byte) {
+		reader := bytes.NewReader(test_file)
+		_, _ = dicom.Parse(reader, int64(len(test_file)), nil)
+	})
+}

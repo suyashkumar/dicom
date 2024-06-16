e9f813a1c945d630269c3cb240d7b5a99c80d7b2
Suyash Kumar <suyashkumar2003@gmail.com>
2024-06-16 00:08:01 -0400

    Improve some errors, rename interface{} to any

--- dataset.go
+++ dataset.go
@@ -33,7 +33,7 @@ func (d *Dataset) FindElementByTag(tag tag.Tag) (*Element, error) {
 			return e, nil
 		}
 	}
-	return nil, ErrorElementNotFound
+	return nil, fmt.Errorf("unable to find %v element: %w", tag, ErrorElementNotFound)
 }
 
 func (d *Dataset) transferSyntax() (binary.ByteOrder, bool, error) {
@@ -57,7 +57,7 @@ func (d *Dataset) FindElementByTagNested(tag tag.Tag) (*Element, error) {
 			return e, nil
 		}
 	}
-	return nil, ErrorElementNotFound
+	return nil, fmt.Errorf("unable to find %v element: %w", tag, ErrorElementNotFound)
 }
 
 // FlatIterator will be deprecated soon in favor of
--- pkg/debug/default.go
+++ pkg/debug/default.go
@@ -1,3 +1,4 @@
+//go:build !debug
 // +build !debug
 
 package debug
@@ -6,4 +7,4 @@ package debug
 func Log(data string) {}
 
 // Logf only logs with a formatted string when the debug build flag is present.
-func Logf(format string, args ...interface{}) {}
+func Logf(format string, args ...any) {}
